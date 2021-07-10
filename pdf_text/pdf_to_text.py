bda0a194d61ea54e5b22b0d9de2690320b7f28da
alxayeed <alxayeed@gmail.com>
2021-07-10 13:32:19 +0600

    working with PyPDF2

--- pdf_text/pdf_to_text.py
+++ pdf_text/pdf_to_text.py
@@ -1,5 +1,6 @@
 import textract
 from pdf2docx import Converter
+import PyPDF2
 
 
 def use_textract(file):
@@ -20,12 +21,29 @@ def use_pdf2docx(file):
         print(row[0], row[1], sep="\t\t\t")
 
 
-def use_pypdf(file):
-    pass
+def pypdf_extract_text(pdf_file):
+    # creating a pdf file object
+    pdf_file_obj = open(pdf_file, 'rb')
+    # creating a PdfFileReader object
+    pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj)
+
+    num_pages = pdf_reader.numPages  # get number of pages
+    pdf_info = pdf_reader.getDocumentInfo()  # get pdf information
+
+    # print(num_pages)
+    # print(pdf_info)
+
+    page_obj = pdf_reader.getPage(2)
+    text = page_obj.extractText()
+    print(text)
+    # closing pdf file object
+    pdf_file_obj.close()
 
 
 if __name__ == "__main__":
-    file_path = 'files/test.pdf'
+    file_path = 'files/example.pdf'
     # output = use_textract(file_path)
-    output = use_pdf2docx(file_path)
+    # output = use_pdf2docx(file_path)
     # print(output)
+
+    pypdf_extract_text(file_path)
